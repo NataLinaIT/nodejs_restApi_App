39e21615a7440868e2ec16a2e50511fcf9c8b561
NataLina <kalipsoN80@gmail.com>
2021-03-27 22:15:10 +0200

    Initial commit

---
+++ backend/app.js
@@ -0,0 +1,52 @@
+const path = require("path");
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const feedRoutes = require("./routes/feed");
+const keys = require("./keys");
+const PORT = process.env.PORT || 8080;
+
+console.log(process.env.NODE_ENV)
+const app = express();
+
+// app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
+app.use(bodyParser.json()); // application/json
+app.use("/images", express.static(path.join(__dirname, "images")));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "OPTIONS, GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+app.use("/feed", feedRoutes);
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  res.status(status).json({ message: message });
+});
+
+async function start() {
+  try {
+    await mongoose.connect(keys.MONGODB_URI, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useFindAndModify: false,
+    });
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+start();
